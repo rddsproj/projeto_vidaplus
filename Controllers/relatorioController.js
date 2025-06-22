0c2cdc5f9195b4318b9b964a4e8342112ab06c36
Ricardo Souza <rdiasdesouza98@gmail.com>
2025-06-22 19:53:38 -0300

    Refatoração do controller de Relatorio

--- Controllers/relatorioController.js
+++ Controllers/relatorioController.js
@@ -6,19 +6,22 @@ const logController = require('./logController')
 module.exports = class relatorioController {
 
     static async relatorioConsultas(req,res){
+        const tokenRole = req.user.role;
 
-        try{
-            const totalConsultas = await Consulta.countDocuments()//conta todos os documentos na collection Consulta, que é o total de consultas no banco de dados
-
-            const porStatus = await Consulta.aggregate([//agrupa as consultas pelo campo status e soma a quantidade
+        if(tokenRole === 'admin'){
+            try{
+            //CONTA A QUANTIDADE DE DOCUMENTOS QUE TEM NA COLLECTION CONSULTA
+            const totalConsultas = await Consulta.countDocuments();
+            //AGRUPA AS CONSULTA PELO CAMPO STATUS E SOMA A QUANTIDADE
+            const porStatus = await Consulta.aggregate([
                 { $group: {_id: "$status", total:{ $sum: 1 }}}
-            ])
-
-            const porEspecialidade = await Consulta.aggregate([//agrupa as consultas pelo campo especialidade e soma a quantidade
+            ]);
+            //AGRUPA AS CONSULTAS PELO CAMPO ESPECIALIDADE E E SOMA A QUANTIDADE
+            const porEspecialidade = await Consulta.aggregate([
                 { $group: {_id: "$especialidade", total:{ $sum: 1 }}}
-            ])
-
-            const porDia = await Consulta.aggregate([//agrupa as consultas pelo campo dataHora e soma a quantidade
+            ]);
+            //AGRUPA AS CONSULTAS PELO CAMPO DATAHORA E SOMA A QUANTIDADE
+            const porDia = await Consulta.aggregate([
                 { $group: {_id: {
                     $dateToString:{
                         format: "%Y-%m-%d",
@@ -28,50 +31,45 @@ module.exports = class relatorioController {
             total: { $sum: 1 }
         }
     }
-            ])
-            const resultado = {//objeto para o resultado
+            ]);
+            //OBJETO PARA O RESULTADO
+            const resultado = {
                 totalConsultas,
                 consultasPorStatus:{},
                 consultasPorEspecialidade:{},
-                consultasPorDia: {}
-            }
+                consultasPorData: {}
+            };
 
-
-            porStatus.forEach(i => {//percorre cada item e preenche o objeto resultado
-                resultado.consultasPorStatus[i._id] = i.total;
-            });
-
-            porEspecialidade.forEach(i => {
-                resultado.consultasPorEspecialidade[i._id] = i.total;
-            });
-
-            porDia.forEach(i => {
-                resultado.consultasPorDia[i._id] = i.total;
-            });
-
-            return res.status(200).json(resultado);//retorna o resultado
+            //PREENCHE O OBJETO
+            porStatus.forEach(i => {resultado.consultasPorStatus[i._id] = i.total;});
+            porEspecialidade.forEach(i => {resultado.consultasPorEspecialidade[i._id] = i.total;});
+            porDia.forEach(i => {resultado.consultasPorData[i._id] = i.total;});
+            return res.status(200).json(resultado);
 
         }catch(err){
             return res.status(500).json({ message: "Erro ao gerar relatório.", error: err.message });
+        }}else{
+            return res.status(403).json({message:"Você não possui permissao para acessar os relatorios de consultas"})
         }
 
     }
 
-
     static async relatorioInternacao(req,res){
-
+        const tokenRole = req.user.role;
+        if(tokenRole === 'admin'){
         try{
-            const totalInternacoes = await Internacao.countDocuments()//conta todos os documentos na collection Consulta, que é o total de consultas no banco de dados
-
-            const porStatus = await Internacao.aggregate([//agrupa as internações por status (internado e alta)
+            //CONTA A QUANTIDADE DE DOCUMENTOS QUE TEM NA COLLECTION INTERNACAO
+            const totalInternacoes = await Internacao.countDocuments()
+            //AGRUPA AS INTERNACOES PELO CAMPO STATUS E SOMA A QUANTIDADE
+            const porStatus = await Internacao.aggregate([
                 { $group: {_id: "$status", total:{ $sum: 1 }}}
             ])
-
-            const porMotivo = await Internacao.aggregate([//agrupa as consultas pelo campo especialidade e soma a quantidade
+            //AGRUPA AS CONSULTAS PELO CAMPO MOTIVO E SOMA A QUANTIDADE
+            const porMotivo = await Internacao.aggregate([
                 { $group: {_id: "$motivo", total:{ $sum: 1 }}}
             ])
-
-            const porDiaEntrada = await Internacao.aggregate([//agrupa as consultas pelo campo dataHora e soma a quantidade
+            //AGRUPA AS CONSULTAS PELA DATA DE ENTRADA E SOMA A QUANTIDADE
+            const porDiaEntrada = await Internacao.aggregate([
                 { $group: {_id: {
                     $dateToString:{
                         format: "%Y-%m-%d",
@@ -82,11 +80,12 @@ module.exports = class relatorioController {
         }
     }
             ])
-
-            const porDiaSaida = await Internacao.aggregate([//agrupa as consultas pelo campo dataHora e soma a quantidade
+            //AGRUPA AS CONSULTAS PELA DATA DE SAIDA E SOMA A QUANTIDADE
+            const porDiaSaida = await Internacao.aggregate([
                 {
                     $match:{
-                        dataSaida:{ $ne:null}//filtra apenas as datas de saidas que não são null
+                        //FILTRA APENAS AS DATAS DE SAIDAS QUE NÃO SÃO NULL
+                        dataSaida:{ $ne:null}
                     }
                 },
                 { $group: {_id: {
@@ -98,70 +97,60 @@ module.exports = class relatorioController {
             total: { $sum: 1 }
         }
     }
-            ])
+            ]);
 
-
-            const resultado = {//objeto para o resultado
+            //OBJETO PARA O RESULTADO
+            const resultado = {
                 totalInternacoes,
                 internacaoPorStatus:{},
                 internacaoPorMotivo:{},
                 internacaoPorEntrada: {},
                 internacaoPorSaida: {}
-            }
+            };
 
+            //PREENCHE O OBJETO
+            porStatus.forEach(i => {resultado.internacaoPorStatus[i._id] = i.total;});
+            porMotivo.forEach(i => {resultado.internacaoPorMotivo[i._id] = i.total;});
+            porDiaEntrada.forEach(i => {resultado.internacaoPorEntrada[i._id] = i.total;});
+            porDiaSaida.forEach(i => {resultado.internacaoPorSaida[i._id] = i.total;});
 
-            porStatus.forEach(i => {//percorre cada item e preenche o objeto resultado
-                resultado.internacaoPorStatus[i._id] = i.total;
-            });
-
-            porMotivo.forEach(i => {
-                resultado.internacaoPorMotivo[i._id] = i.total;
-            });
-
-            porDiaEntrada.forEach(i => {
-                resultado.internacaoPorEntrada[i._id] = i.total;
-            });
-
-            porDiaSaida.forEach(i => {
-                resultado.internacaoPorSaida[i._id] = i.total;
-            });
-
-            return res.status(200).json(resultado);//retorna o resultado
+            return res.status(200).json(resultado);
 
         }catch(err){
             return res.status(500).json({ message: "Erro ao gerar relatório.", error: err.message });
         }
-
+        }else{
+            return res.status(403).json({message:"Você não possui permissao para acessar os relatorios de Internações"})
+        }
     }
 
     static async relatorioLeito(req,res){
+        const tokenRole = req.user.role;
 
+        if(tokenRole === 'admin'){
         try{
-            const totalLeitos= await Leito.countDocuments()//conta todos os documentos na collection Consulta, que é o total de consultas no banco de dados
-
-            const porStatus = await Leito.aggregate([//agrupa as consultas pelo campo status e soma a quantidade
+            //CONTA A QUANTIDADE DE DOCUMENTOS QUE TEM NA COLLECTION LEITO
+            const totalLeitos= await Leito.countDocuments();
+            //AGRUPA AS CONSULTA PELO CAMPO STATUS E SOMA A QUANTIDADE
+            const porStatus = await Leito.aggregate([
                 { $group: {_id: "$status", total:{ $sum: 1 }}}
-            ])
+            ]);
 
-
-
-
-            const resultado = {//objeto para o resultado
+            //OBJETO PARA O RESULTADO
+            const resultado = {
                 totalLeitos,
                 leitosPorStatus:{}
-            }
-
-
-            porStatus.forEach(i => {//percorre cada item e preenche o objeto resultado
-                resultado.leitosPorStatus[i._id] = i.total;
-            })
-
-            return res.status(200).json(resultado);//retorna o resultado
+            };
+            //PREENCHE O OBJETO
+            porStatus.forEach(i => {resultado.leitosPorStatus[i._id] = i.total;});
+            return res.status(200).json(resultado);
 
         }catch(err){
             return res.status(500).json({ message: "Erro ao gerar relatório.", error: err.message });
         }
 
-    }
+    }else{
+            return res.status(403).json({message:"Você não possui permissao para acessar os relatorios de Leitos"})
+        }
 
-}
+}}
